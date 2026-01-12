42ee602e9e1783de9011c0535323ec279d949274
Zhentao-Lin <173039613@qq.com>
2026-01-12 15:46:02 +0800

    Update servo.py

--- Server/Code/servo.py
+++ Server/Code/servo.py
@@ -1,15 +1,37 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 
-from gpiozero import PWMOutputDevice  
 import time  
-import subprocess
+
+def get_raspberry_pi_model():  
+    try:  
+        with open('/proc/cpuinfo', 'r') as f:  
+            cpuinfo = f.readlines()  
+        for line in cpuinfo:  
+            if line.startswith('Model'):  
+                model_info = line.strip().split(':')[-1].strip().split("Model")[0].strip()
+                return model_info  
+        return 'Unknown Raspberry Pi Model'  
+    except Exception as e:  
+        print(f"Error reading /proc/cpuinfo: {e}")  
+        return 'Error Reading' 
+
+pi_model = get_raspberry_pi_model()
+if "Pi 5" in pi_model:
+    from piolib_servo import PiolibServo
+    library_type = "piolib"
+else:
+    from gpiozero_servo import GpiozeroServo
+    library_type = "gpiozero"
 
 class Servo:
     def __init__(self):
-        self.SERVO_PIN = [13,16,19,20,26] 
-        self.servo = [PWMOutputDevice(pin, initial_value=0) for pin in self.SERVO_PIN]  
-
+        if library_type == "piolib":
+            self.servo = PiolibServo()
+        else:
+            self.servo = GpiozeroServo()
+        self.SERVO_PIN = [13, 16, 19, 20, 26]
+        
     def constrain(self, value, min, max):
         if value > max:
             value = max
@@ -18,17 +40,13 @@ class Servo:
         return value
     
     def setServoAngle(self, index, angle):
-        if index < len(self.SERVO_PIN):
-            angle = self.constrain(angle, 0, 180)
-            servo_duty = 500+(2000/180)*angle
-            self.servo[index].frequency = 50 
-            self.servo[index].value = servo_duty/20000
-            return servo_duty
-    def relaxServo(self, index):
-        self.servo[index].off()
+        servo_duty = self.servo.setServoAngle(self.SERVO_PIN[index], angle)
+        return servo_duty
+    def relaxServo(self):
+        self.servo.relaxServo()
     
     def servoClose(self):
-        [self.servo[i].close() for i in range(len(self.SERVO_PIN))]
+        self.servo.servoClose()
 
 # Main program logic follows:
 if __name__ == '__main__':
@@ -40,14 +58,14 @@ if __name__ == '__main__':
     try:
         while True:
             if len(sys.argv)==1:
-                for j in range(0, 180, 1):
-                    for i in range(5):
+                for i in range(5):
+                    print("Servo index:", i)
+                    for j in range(0, 180, 10):
                         servo.setServoAngle(i, j)
-                    time.sleep(0.1)
-                for j in range(0, 180, 1):
-                    for i in range(5):  
+                        time.sleep(0.05)
+                    for j in range(0, 180, 10):
                         servo.setServoAngle(i, 180-j)
-                    time.sleep(0.1)
+                        time.sleep(0.05)
             elif len(sys.argv)==2:
                 index = servo.constrain(int(sys.argv[1]), 0, 4)
                 servo.setServoAngle(index, 90)
@@ -59,6 +77,5 @@ if __name__ == '__main__':
                 time.sleep(0.1)
             
     except KeyboardInterrupt:
-        for i in range(5):
-            servo.relaxServo(i)
+        servo.relaxServo()
         time.sleep(0.5)
